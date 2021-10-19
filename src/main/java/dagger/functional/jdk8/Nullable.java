805f34453e098d4a2ebdf05ce8b30753dc1690d8
jbock <jbock-java@gmx.de>
2021-10-19 15:11:32 +0200

    issue #1, add tests from package jdk8

---
+++ src/main/java/dagger/functional/jdk8/Nullable.java
@@ -0,0 +1,10 @@
+package dagger.functional.jdk8;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Nullable {
+}
